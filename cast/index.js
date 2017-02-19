4ea8f2b896641ebc623b2d01544a653b14bb4624
thai <thainam83vn@gmail.com>
2017-02-19 06:00:51 -0800

    u

--- cast/index.js
+++ cast/index.js
@@ -3,7 +3,8 @@ var app = express();
 
 // Define the port to run on
 app.set('port', 3000);
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static('public'));
 
 // Listen for requests
 var server = app.listen(app.get('port'), function() {
